7fe57a59f0b4fb5571e699137d7506739343b0d3
AndreSoffiantini <andrea.soffiantini@hotmail.it>
2022-10-30 17:50:40 +0100

    Add base structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="container">
+        <div className="col-2 flex-center">
+          <div className="text-box text-white">
+            <h1> Learn to code by watching others</h1>
+            <p>
+              See how experienced developers solve problems in real-time.
+              Watching scripted tutorials is great, but understanding how
+              developers think is invaluable.
+            </p>
+          </div>
+        </div>
+
+        <div className="col-2 flex-col justify-between">
+          <div></div>
+          <div
+            className="rounded-square bg-blue text-white"
+            style={{ height: "15%" }}
+          >
+            Try it free 7 days then $20/mo. thereafter
+          </div>
+          <div className="rounded-square bg-white" style={{ height: "80%" }}>
+            <div>
+              <form>
+                <div className="form-fields">
+                  <div className="form-field">
+                    <input type="text" placeholder="First Name" />
+                  </div>
+                  <div className="form-field">
+                    <input type="text" placeholder="Last Name" />
+                  </div>
+                  <div className="form-field">
+                    <input type="email" placeholder="Email Address" />
+                  </div>
+                  <div className="form-field">
+                    <input type="password" placeholder="Password" />
+                  </div>
+                </div>
+                <div className="form-button">
+                  <button type="submit" style={{ color: "white" }}>
+                    Claim your free trial
+                  </button>
+                </div>
+              </form>
+              <p>
+                By clicking the button, you are agreeing to our Terms and
+                Services
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
